ffcab6d70c1e43ebaa81489a6f2714b210d38744
tembhurnerohit2194 <tembhurnerohit2194@gmail.com>
2025-05-20 10:46:52 +0530

    Comit By Rohit

--- Models/LIVEAdvanceDevSwachhBharatMainContext.cs
+++ Models/LIVEAdvanceDevSwachhBharatMainContext.cs
@@ -23,7 +23,7 @@ namespace DataEntryICTSBM.Models
             if (!optionsBuilder.IsConfigured)
             {
                 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=43.242.215.209;Initial Catalog=LIVEAdvanceDevSwachhBharatMain;User Id=appynitty; Password=BigV$Telecom;Connection Timeout=3600;");
+                optionsBuilder.UseSqlServer("Data Source=172.16.0.90;Initial Catalog=LIVEAdvanceDevSwachhBharatMain;User Id=sa; Password=Bi9vAppy;Connection Timeout=3600;");
             }
         }
     }
